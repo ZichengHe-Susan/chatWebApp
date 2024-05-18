62ecd1253eaa818139e6ff7dae8720accb4eb9a3
ZichengHe-Susan <bgc4bx@virginia.edu>
2024-05-17 20:19:13 -0400

    enabled registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import {
+  BrowserRouter as BroswerRouter,
+  Routes,
+  Route,
+}from "react-router-dom";
 
 import "./style.scss"
 function App() {
-  return (<Home/>);
+  return (
+    <BroswerRouter>
+      <Routes>
+        <Route path= "/">
+          <Route index element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BroswerRouter>
+  );
 }
 
 export default App;
