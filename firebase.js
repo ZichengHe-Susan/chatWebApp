45d845d244c99336b49d1e9151586ed262d9df04
ZichengHe-Susan <bgc4bx@virginia.edu>
2024-05-20 16:34:10 -0400

    push

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyAdlSwf7Gj0iKihCUYgANZmE1ze-rQ-dqk",
   authDomain: "chat-33c5a.firebaseapp.com",
